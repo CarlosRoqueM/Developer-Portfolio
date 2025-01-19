7414fd7d8418fe16d5bd0f1e458ead7ce818bf22
CarlosRoqueM <carlos.roque@tecsup.edu.pe>
2025-01-19 18:35:52 -0500

    add animation home and cv contact

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from './Layout'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <footer>
+        <Layout>
+            <span>
+                2025 &copy;  All Rights Reserved.
+            </span>
+            <Link href="/">HOLA</Link>
+            <Link href="/">HOLA</Link>
+        </Layout>
+    </footer>
+  )
+}
+
+export default Footer
+
