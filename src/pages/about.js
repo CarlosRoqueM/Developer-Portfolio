7170a3882d83d5c71b5242daa7f2b30f5231f4c6
CarlosRoqueM <carlos.roque@tecsup.edu.pe>
2025-01-21 23:08:19 -0500

    add div to foto perfil

--- src/pages/about.js
+++ src/pages/about.js
@@ -29,9 +29,9 @@ const about = () => {
               </p>
             </div>
 
-              <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
+            <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
+              <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-2xl bg-dark' />
                 <Image src={profilePic} atl='Carlos Roque' className='w-full h-auto rounded-2xl'/>
-
             </div>
           </div>
         </Layout>
